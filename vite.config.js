c64c9fb93edcbc257bcdc8e3c982c6f82d714017
vlad <vnelipovich63@gmail.com>
2024-09-07 15:54:09 +0300

    fix: netlify

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,5 @@
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [react()],
   base: '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 })
